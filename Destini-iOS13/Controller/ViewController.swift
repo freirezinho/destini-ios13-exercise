503565653e510cf928d9e6534cd85d9f9caf7ab0
saulosf <gbsaulo@gmail.com>
2020-03-03 00:51:48 -0300

    Constants + layout

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = story0
 
     }
 
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
+    let story0: String = "You see a fork in the road"
+    let choice1: String = "Take a left"
+    let choice2: String = "Take a right"
 }
 
