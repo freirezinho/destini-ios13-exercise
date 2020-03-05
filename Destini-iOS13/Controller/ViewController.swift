0d6e5209218ad3470acfb347cb458c9b5a584675
saulosf <gbsaulo@gmail.com>
2020-03-05 01:07:47 -0300

    Storybrain struct WIP

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -24,14 +24,31 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var choice2Button: UIButton!
     
-    @IBAction func choiceMade(_ sender: UIButton) {
-    }
     
     let firstStory = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
     let secondStory = Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead.")
     let thirdStory = Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
 
     lazy var stories = [firstStory, secondStory, thirdStory]
+    
+    var storyIndex: Int = 0
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let title = sender.title(for: .normal)
+        if title == stories[0].choice1 {
+            storyIndex = 1
+        } else if title == stories[0].choice2{
+            storyIndex = 2
+        } else {
+            storyIndex = 0
+        }
+        updateUI()
+    }
+    func updateUI() {
+        storyLabel.text = stories[storyIndex].title
+        choice1Button.setTitle(stories[storyIndex].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyIndex].choice2, for: .normal)
+    }
     
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,28 @@
 
 import Foundation
 
+struct StoryBrain {
+    var stories: [Story] = [
+    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+    Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    var storyIndex: Int = 0;
+    
+    mutating func createStory(title: String, choice1: String, choice2: String) {
+        let story = Story(title: title, choice1: choice1, choice2: choice2)
+        stories.append(story)
+    }
+    
+    mutating func nextStory(userChoice: String) {
+        let title = userChoice
+        if title == stories[0].choice1 {
+            storyIndex = 1
+        } else if title == stories[0].choice2{
+            storyIndex = 2
+        } else {
+            storyIndex = 0
+        }
 
+    }
+}
